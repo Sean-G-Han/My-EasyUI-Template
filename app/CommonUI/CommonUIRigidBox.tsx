b21c345ef0d6d1a104aa62aadfa89a3ae75e7e4b
Sean-G-Han <156769273+Sean-G-Han@users.noreply.github.com>
2025-10-12 01:46:28 +0800

    Add Style to RigidBox

--- app/CommonUI/CommonUIRigidBox.tsx
+++ app/CommonUI/CommonUIRigidBox.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native'
+import { StyleProp, StyleSheet, ViewStyle } from "react-native";
 import * as XYWH from "../xywh";
 import React from 'react'
 import CommonUIBox from './CommonUIBox';
@@ -7,12 +7,13 @@ export type Props = {
     xywh: XYWH.XYWH;
     children?: React.ReactNode;
     padding?: number;
+    style?: StyleProp<ViewStyle>;
 };
 
-const CommonUIRigidBox = ({ xywh, children, padding }: Props) => {
+const CommonUIRigidBox = ({ xywh, children, padding, style }: Props) => {
   const position: XYWH.XYWH = XYWH.convertToTopLeft(xywh);
   return (
-    <CommonUIBox width={position.width} height={position.height} padding={padding || 0} style={{...styles.container, left: position.x, top: position.y }}>
+    <CommonUIBox width={position.width} height={position.height} padding={padding || 0} style={[{...styles.container, left: position.x, top: position.y}, style]}>
         {children}
     </CommonUIBox>
   )
--- app/index.tsx
+++ app/index.tsx
@@ -17,6 +17,34 @@ const Home = () => {
     parent, 
     XYWH.referencePoint.CENTER
   );
+  const lefteye = XYWH.attachTo(
+    XYWH.create(0, 0, 50, 50, XYWH.referencePoint.TOP_LEFT),
+    XYWH.referencePoint.BOTTOM_LEFT,
+    child,
+    XYWH.referencePoint.TOP_LEFT,
+    0,
+    -30
+  );
+  const leftPupil = XYWH.attachTo(
+    XYWH.create(0, 0, 20, 20, XYWH.referencePoint.TOP_LEFT),
+    XYWH.referencePoint.CENTER,
+    lefteye,
+    XYWH.referencePoint.CENTER
+  );
+  const righteye = XYWH.attachTo( 
+    XYWH.create(0, 0, 50, 50, XYWH.referencePoint.TOP_LEFT),
+    XYWH.referencePoint.BOTTOM_RIGHT,
+    child,
+    XYWH.referencePoint.TOP_RIGHT,
+    0,
+    -30
+  );
+  const rightPupil = XYWH.attachTo(
+    XYWH.create(0, 0, 20, 20, XYWH.referencePoint.TOP_LEFT),
+    XYWH.referencePoint.CENTER,
+    righteye,
+    XYWH.referencePoint.CENTER
+  );
   return (
     <CommonUIRigidBox xywh={parent}>
       <CommonUIRigidBox padding={10} xywh={child}>
@@ -24,6 +52,10 @@ const Home = () => {
         <CommonUITextField placeholder='Password'/>
         <CommonUIButton text='Login' onPress={() => {}}/>
       </CommonUIRigidBox>
+      <CommonUIRigidBox xywh={lefteye} />
+      <CommonUIRigidBox xywh={righteye} />
+      <CommonUIRigidBox xywh={leftPupil} style={{ backgroundColor: 'black' }} />
+      <CommonUIRigidBox xywh={rightPupil} style={{ backgroundColor: 'black' }} />
     </CommonUIRigidBox>
   )
 }
